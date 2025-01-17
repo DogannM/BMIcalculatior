2987798e275fac5119c41bc7583f7673e0ec582f
DogannM <canmrnc2620@gmail.com>
2025-01-17 20:44:22 +0300

    Correction Commit.

--- BMI calculate.py
+++ BMI calculate.py
@@ -4,3 +4,13 @@ x = float(x)
 y = float(y)
 bmi = x / y ** 2
 print("Your BMI is : ", bmi)
+
+if bmi < 18.5:
+    print("You're Underweight")
+elif 18.5 <= bmi <= 25:
+    print("You're Normal")
+elif 25 < bmi <= 30:
+    print("You're Overweight")
+else:
+    print("You're Obese")
+    
