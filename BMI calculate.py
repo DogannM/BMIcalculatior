a463435763f2da57c5d2a69e91ae99ac35592eea
DogannM <canmrnc2620@gmail.com>
2025-01-17 20:35:57 +0300

    Classification statement merged Calculatior.

--- BMI calculate.py
+++ BMI calculate.py
@@ -1,8 +1,6 @@
-import math
-
 x = input("Enter your weight in kg : ")
 y = input("Enter your height in cm : ")
 x = float(x)
 y = float(y)
 bmi = x / y ** 2
-print("Your BMI is : ", bmı)
+print("Your BMI is : ", bmi)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,6 @@
-import math
-
 x = input("Enter your weight in kg : ")
 y = input("Enter your height in cm : ")
 x = float(x)
 y = float(y)
-bmı = x / y ** 2
-print("Your BMI is : ", bmı)
+bmi = x / y ** 2
+print("Your BMI is : ", bmi)
