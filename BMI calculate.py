8f1bbc325650f187e37d138a4ea09eda86af388c
DogannM <canmrnc2620@gmail.com>
2025-01-17 20:32:30 +0300

    Classification function created.

--- BMI calculate.py
+++ BMI calculate.py
@@ -1,4 +1,8 @@
+import math
+
 x = input("Enter your weight in kg : ")
 y = input("Enter your height in cm : ")
-bmı = x / y**2
+x = float(x)
+y = float(y)
+bmi = x / y ** 2
 print("Your BMI is : ", bmı)
---
+++ sorter func.py
@@ -0,0 +1,10 @@
+bmi = 0
+if bmi < 18.5:
+    print("You're Underweight"),
+elif 18.5 <= bmi <= 25:
+    print("You're Normal"),
+elif 25 < bmi <= 30:
+    print("You're Overweight"),
+else:
+    print("You're Obese"),
+    
